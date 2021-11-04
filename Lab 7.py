b9022bce25e792f739645a3ab840d8f802f1f2f9
anthonydelvecchio <amdelvecchio@loyola.edu>
2021-11-04 10:35:47 -0400

    started defining functions

---
+++ Lab 7.py
@@ -0,0 +1,12 @@
+# Anthony DelVecchio
+# Course: CS151, Dr. Rajeev
+# Date: 11/4/21
+# Lab Number: 7
+# Program Inputs: The number of rolls of 2 dice
+# Program Outputs: The total number between the 2 dice for every roll
+
+# Define Functions
+def get_number_of_rolls():
+    number_of_rolls = input("Input Number of Rolls: ")
+    int(number_of_rolls)
+    return number_of_rolls
